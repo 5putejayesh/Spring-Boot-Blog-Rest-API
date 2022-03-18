bf410b76aa3d2c4527ddb0dcedcaec412157e814
Jayesh 5pute <5putejayesh@gmail.com>
2022-03-18 16:31:21 +0530

    test cases added

--- src/test/java/com/jayesh/springboot/blog/repository/PostRepositoryTest.java
+++ src/test/java/com/jayesh/springboot/blog/repository/PostRepositoryTest.java
@@ -1,44 +1,71 @@
 package com.jayesh.springboot.blog.repository;
 
 import com.jayesh.springboot.blog.entity.Post;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 public class PostRepositoryTest {
     @Autowired
-    private TestEntityManager testEntityManager;
-    @MockBean
     private PostRepository postRepository;
 
+    private static Post post;
+    private Post persist;
+    @BeforeEach
+    public void setup(){
+        post=getPost();
+    }
     @Test
-    public void testSavePost(){
-
-        Post post=getPost();
-        Post persist = testEntityManager.persist(post);
+    public void testSavePost_NotNull(){
+         persist = postRepository.save(post);
         assertNotNull(persist.getId());
+    }
+
+    @Test
+    public void testSavePost_equals(){
+        Post savedPost = postRepository.save(post);
+        Optional<Post> byId = postRepository.findById(savedPost.getId());
+        assertEquals(savedPost,byId.get());
+    }
+    @Test
+    public void testSavePost_sameTile(){
+        Post post = postRepository.save(PostRepositoryTest.post);
+
+        assertThrows(Exception.class,()->{
+            postRepository.save(getPost());
+        });
+    }
+
+    @Test
+    public void testGetPostById_sameId(){
+        Post savedPost = postRepository.save(post);
+        
     }
 
     private Post getPost(){
 
-        Post post = new Post();
+         post = new Post();
         post.setTitle("Junit Test Post");
         post.setContent("Junit Test Post Content");
         post.setDescription("Junit Test Post Description");
 
         return post;
+    }
+
+    @AfterAll
+    public static void cleanup(){
+        post=null;
     }
 
 }

8433091d89e7da1d238b8e2de950f249acc2c574
Jayesh 5pute <5putejayesh@gmail.com>
2022-03-18 17:37:33 +0530

    test cases added

--- src/test/java/com/jayesh/springboot/blog/repository/PostRepositoryTest.java
+++ src/test/java/com/jayesh/springboot/blog/repository/PostRepositoryTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,7 +24,7 @@ public class PostRepositoryTest {
     private Post persist;
     @BeforeEach
     public void setup(){
-        post=getPost();
+        post=getPost("Junit Test Post","Junit Test Post","Junit Test Post");
     }
     @Test
     public void testSavePost_NotNull(){
@@ -43,24 +43,71 @@ public class PostRepositoryTest {
         Post post = postRepository.save(PostRepositoryTest.post);
 
         assertThrows(Exception.class,()->{
-            postRepository.save(getPost());
+            postRepository.save(getPost("Junit Test Post","Junit Test Post","Junit Test Post"));
         });
     }
 
     @Test
+    public void testGetPostById_diffId(){
+        Post savedPost = postRepository.save(post);
+        Post post = postRepository.findById(savedPost.getId()).get();
+        assertThrows(Exception.class,()->postRepository.findById(10L).get());
+    }
+    @Test
     public void testGetPostById_sameId(){
         Post savedPost = postRepository.save(post);
-        
+        Post post = postRepository.findById(savedPost.getId()).get();
+        assertNotNull(post);
+        assertEquals(savedPost.getId(),post.getId());
+        assertDoesNotThrow(()->postRepository.findById(1L));
     }
 
-    private Post getPost(){
+    @Test
+    public void testUpdatePost(){
+        postRepository.save(post);
+        Post savedPost = postRepository.findById(post.getId()).get();
+        savedPost.setTitle("Post1");
+        Post updPost = postRepository.save(savedPost);
+        assertThat(updPost.getTitle()).isEqualTo("Post1");
+    }
+    @Test
+    public void testDeletePOst(){
+
+        //save post
+        postRepository.save(post);
+
+        //delete post
+        postRepository.deleteById(post.getId());
+        //retrieve post
+        Optional<Post> byId = postRepository.findById(post.getId());
+        assertThat(byId).isEmpty();
+    }
+    public void testFindAllPost(){
+
+        List<Post> postList = getPosts();
+        List<Post> saveAll = postRepository.saveAll(postList);
+        assertThat(saveAll).isNotNull();
+        assertThat(saveAll).isNotEmpty();
+        assertThat(saveAll.size()).isEqualTo(2);
+    }
+
+    private Post getPost(String title,String content,String description){
 
          post = new Post();
-        post.setTitle("Junit Test Post");
-        post.setContent("Junit Test Post Content");
-        post.setDescription("Junit Test Post Description");
+        post.setTitle(title);
+        post.setContent(content);
+        post.setDescription(description);
 
         return post;
+    }
+
+
+
+    private List<Post> getPosts(){
+        return Arrays.asList(
+                getPost("Junit Test Post","Junit Test Post","Junit Test Post"),
+                getPost("Second Post","Second Post","Second Post")
+        );
     }
 
     @AfterAll
